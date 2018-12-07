25962f4cae813ffdcd61e32cdb934ecc01b190e0
77ccreed <kiur.jullis@gmail.com>
2018-12-07 15:07:28 +0200

    smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-// Note: A custom script is used to activate tooltips:
+// A custom script is used to activate tooltips:
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top - 56)
+      }, 1000, "easeInOutExpo");
+      return false;
+    }
+  }
 });
