ddcb0d467f73628eea0956cbe1b1144bf7126291
77ccreed <kiur.jullis@gmail.com>
2018-12-22 12:17:26 +0200

    ..

--- js/app.js
+++ js/app.js
@@ -9,15 +9,10 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: (target.offset().top - 56)
-      }, 1000, "easeInOutExpo");
-      return false;
-    }
   }
 });
 
+// Hide btn-up on top
 var offsetAmount = 500;
 $(document).scroll(function () {
   if ($(document).scrollTop() >= offsetAmount) {
