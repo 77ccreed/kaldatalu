b546813e19e0c6bc6e7dd0eba42ffc10b9c552dd
77ccreed <kiur.jullis@gmail.com>
2019-03-23 08:48:35 +0200

    kasutamata kood eemaldatud. sw välja kommenteeritud

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-document.addEventListener('DOMContentLoaded', (event) => { if ('serviceWorker' in navigator) { navigator.serviceWorker.register('/kaldatalu/js/service-worker.js').then((registration) => { console.log('ServiceWorker registration successful with scope: ', registration.scope) }, (err) => { console.log('ServiceWorker registration failed: ', err) }) } }); $(function () { $('[data-toggle="tooltip"]').tooltip() }); $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () { if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) { var target = $(this.hash); target = target.length ? target : $('[name=' + this.hash.slice(1) + ']') } }); var offsetAmount = 500; $(document).scroll(function () { if ($(document).scrollTop() >= offsetAmount) { $('.scroll-hide').removeClass("d-none") } else { $('.scroll-hide').addClass("d-none") } }); $(function () { $(document).scroll(function () { var $nav = $(".navbar-fixed-top"); $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height()) }) })
+/*document.addEventListener('DOMContentLoaded', (event) => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/kaldatalu/js/service-worker.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    }, (err) => {
+      console.log('ServiceWorker registration failed: ', err)
+    })
+  }
+});*/
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+var offsetAmount = 500;
+$(document).scroll(function () {
+  if ($(document).scrollTop() >= offsetAmount) {
+    $('.scroll-hide').removeClass("d-none")
+  } else {
+    $('.scroll-hide').addClass("d-none")
+  }
+});
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height())
+  })
+})
+
+
